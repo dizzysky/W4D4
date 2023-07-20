c2be08e32984d5c8caa1564d713aee4c58a12628
Sanjid <dsanjid@gmail.com>
2023-07-20 15:39:41 -0400

    finished anagrams

--- practice-for-ch-ruby-anagrams-long-practice-main/anagram.rb
+++ practice-for-ch-ruby-anagrams-long-practice-main/anagram.rb
@@ -35,14 +35,50 @@ end
 def second_anagram?(string1, string2)
     string1.each_char do |char|
         if string2.chars.find_index(char) != nil
-            p char + " " + string2
-            string2 = string2.chars.delete_at(string2.chars.find_index(char))
+            string2 = string2.chars
+            string2.delete_at(string2.find_index(char))
+            string2 = string2.join
         end
 
     end
-    p string2
+    
     string2.empty?
 end
 
-p second_anagram?("gizmo","sally")
-p second_anagram?("elvis", "lives")
+
+# p 
+# p first_anagram?("basiparachromatin","marsipobranchiata")
+# p second_anagram?("basiparachromatin", "marsipobranchiata")
+
+
+
+def third_anagram?(string1,string2)
+    sorted_1 = jumble_sort(string1)
+    sorted_2 = jumble_sort(string2)
+
+    sorted_1 == sorted_2
+
+end # O(nlogn)
+
+def jumble_sort(str)
+    alphabet = ('a'..'z').to_a
+    sorted = false
+    until sorted
+        sorted = true
+        (0...str.length - 1).to_a.each do |i|
+            if alphabet.index(str[i]) > alphabet.index(str[i + 1])
+                str[i], str[i + 1] = str[i + 1], str[i]
+                sorted = false
+            end
+        end
+    end
+    str
+end   
+
+
+p third_anagram?("gizmo", "sally")
+p third_anagram?("elvis", "lives")
+
+
+
+
